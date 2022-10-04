49b4f0e3375d5066cb30eddb895718eb93af76a5
LPastine <lucianopastine.tech@gmail.com>
2022-10-04 16:22:13 -0300

    add first endpoint

---
+++ src/server.ts
@@ -0,0 +1,41 @@
+import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+import cors from "koa2-cors";
+import logger from "koa-logger";
+import Router from "koa-router";
+
+const app = new Koa();
+
+const PORT = process.env.PORT || 7654;
+
+app.use(bodyParser());
+app.use(
+  cors({
+    origin: "*"
+  })
+);
+app.use(logger());
+
+const router = new Router();
+
+router.get(`/`, async (ctx) => {
+  try {
+    ctx.body = {
+      status: "success",
+    };
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+app.use(router.routes());
+
+const server = app
+  .listen(PORT, async () => {
+    console.log(`Server listening on port: ${PORT}`);
+  })
+  .on("error", err => {
+    console.error(err);
+  });
+
+export default server;
