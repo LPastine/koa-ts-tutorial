2402bc1f3f1364b4556a09a89e32fde3e83a0ab5
LPastine <lucianopastine.tech@gmail.com>
2022-10-05 10:16:02 -0300

    adding redis

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
-interface IConfig {
-  port: string;
-}
+import * as Interfaces from "./types/interfaces";
 
-export const config: IConfig = {
+export const config: Interfaces.IConfig = {
   port: process.env.PORT || "7654",
+  redis: {
+    host: process.env.REDIS__HOST || "0.0.0.0",
+    port: process.env.REDIS__PORT || "6401",
+  }
 }
